127e1f57bde745823c102a1403b9fbb6e915ca93
Daniel Smith <danoz73@gmail.com>
2013-12-15 05:26:36 -0800

    built with gradle for android studio

---
+++ build/source/buildConfig/debug/com/qustom/dialog/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.qustom.dialog;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
