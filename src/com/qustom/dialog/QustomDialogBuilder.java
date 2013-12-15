5aae2bf1648dc5fe0d5c999b3d04623210a60c2f
Daniel Smith <danoz73@gmail.com>
2013-12-15 06:11:06 -0800

    Add items below custom frame
     - currently unclickable
     - no dividers

--- src/com/qustom/dialog/QustomDialogBuilder.java
+++ src/com/qustom/dialog/QustomDialogBuilder.java
@@ -2,19 +2,20 @@ package com.qustom.dialog;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.qustom.dialog.R;
 
 public class QustomDialogBuilder extends AlertDialog.Builder{
 
 	/** The custom_body layout */
 	private View mDialogView;
+    private AlertDialog mDialog;
 	
 	/** optional dialog title layout */
 	private TextView mTitle;
@@ -96,11 +97,64 @@ public class QustomDialogBuilder extends AlertDialog.Builder{
     	((FrameLayout)mDialogView.findViewById(R.id.customPanel)).addView(customView);
     	return this;
     }
+
+    /**
+     * Set a list of items to be displayed in the dialog as the content, you will be notified of the
+     * selected item via the supplied listener. This should be an array type i.e. R.array.foo
+     *
+     * @return This Builder object to allow for chaining of calls to set methods
+     */
+    @Override
+    public QustomDialogBuilder setItems(int itemsId, final DialogInterface.OnClickListener listener) {
+        return setItems(getContext().getResources().getTextArray(itemsId), listener);
+    }
+
+    /**
+     * Set a list of items to be displayed in the dialog as the content, you will be notified of the
+     * selected item via the supplied listener.
+     *
+     * @return This Builder object to allow for chaining of calls to set methods
+     */
+    @Override
+    public QustomDialogBuilder setItems(CharSequence[] items, final DialogInterface.OnClickListener listener) {
+        LinearLayout itemList = (LinearLayout) mDialogView.findViewById(R.id.items_list);
+
+        for (int i = 0; i < items.length; i++) {
+            final int currentItem = i;
+            View listItem = inflateItem(items[i].toString());
+            itemList.addView(listItem);
+            if (listener != null) {
+                itemList.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        listener.onClick(mDialog, currentItem);
+                    }
+                });
+            }
+        }
+
+        return this;
+    }
+
+    private View inflateItem(String itemText) {
+        View listItem = View.inflate(getContext(), R.layout.qustom_dialog_item_layout, null);
+        TextView bestFriendNameTextView = (TextView) listItem.findViewById(R.id.item_text);
+        bestFriendNameTextView.setText(itemText);
+        return listItem;
+    }
+
+    private View inflateDivider(String itemText) {
+        View listItem = View.inflate(getContext(), R.layout.qustom_dialog_item_layout, null);
+        TextView bestFriendNameTextView = (TextView) listItem.findViewById(R.id.item_text);
+        bestFriendNameTextView.setText(itemText);
+        return listItem;
+    }
     
     @Override
     public AlertDialog show() {
     	if (mTitle.getText().equals("")) mDialogView.findViewById(R.id.topPanel).setVisibility(View.GONE);
-    	return super.show();
+    	mDialog = super.show();
+        return mDialog;
     }
 
 }
