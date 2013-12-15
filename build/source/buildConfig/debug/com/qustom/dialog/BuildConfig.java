a24c787bf61c590c6b9bdeed20dc6784625a4d0a
Daniel Smith <danoz73@gmail.com>
2013-12-15 06:08:20 -0800

    ignoring build and gradle folders

--- build/source/buildConfig/debug/com/qustom/dialog/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.qustom.dialog;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
